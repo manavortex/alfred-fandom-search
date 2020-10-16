915923cf3665874f0f6ea47d94993a2a7c8573e8
manavortex <manavortex@gmail.com>
2020-10-16 17:49:14 +0200

    adjustments to new api

--- 00_runme.py
+++ 00_runme.py
@@ -5,17 +5,21 @@ import sys
 import requests 
 import urllib3
 import json
-from urllib import quote
+from urllib import (quote, urlencode)
 from workflow import (Workflow3, ICON_INFO, ICON_WARNING, ICON_ERROR, ICON_WEB,
                       ICON_SETTINGS, ICON_SYNC)
 from bs4 import BeautifulSoup
 
+# =================================================================================== #
 # change this to the name of the wiki you want to query, e.g. 'lyrics', 'witcher'...
 DEFAULT_WIKI = ""
+# =================================================================================== #
 
 
-# do not change this
-DEFAULT_URL = "https://%s.fandom.com/api/v1/Search/List"
+# =================================================================================== #
+# DO NOT CHANGE BELOW THIS LINE UNLESS YOU KNOW WHAT YOU'RE DOING :)
+# =================================================================================== #
+DEFAULT_URL = "https://%s.fandom.com/api.php?"
 
 class AlfredFandomSearch(object):
     """Workflow controller."""
@@ -23,20 +27,36 @@ class AlfredFandomSearch(object):
     def __init__(self):
         self.wf = Workflow3(update_settings={'github_slug': 'manavortex/alfred-fandom-search'})
 
+    
+
     def search(self):
         S = requests.Session()
         PARAMS = {
-            "action": sys.argv[1],
+            "action": 'query',
+            "list": 'search',            
             "format": "json",
-            "limit": "10",
-            "batches": "1",
-            "minArticleQuality": "5", 
+            'prop': 'snippet',
+            #"limit": "10",
+            #"batches": "1",
+            #"redirects": "resolve",
+            #"minArticleQuality": "5", 
             "namespace": "0%2C14",
-            "query": self.query
+            "srsearch": self.query,
         }
-
-        R = S.get(url=self.url, params=PARAMS)
-        return R.json()["items"]
+        URL = url=self.url+urlencode(PARAMS)
+        #print(URL)        
+        R = requests.get(url=URL)
+        #print(R.json())
+        try:
+            return R.json()['items']
+        except Exception as e:
+            try:
+                return R.json()['query']['search']
+            except Exception as e:
+                print("\n\nException!!")
+                print(e)
+                print(R.json())
+        return []
 
     def run(self, wf):
         """Run workflow."""
@@ -45,7 +65,7 @@ class AlfredFandomSearch(object):
         
         if DEFAULT_WIKI == "": 
             self.wf.add_item(
-                "Error: DEFAULT_WIKI in script filter not set", 
+                "Error: DEFAULT_URL in script filter not set", 
                 subtitle="press enter to see readme", 
                 arg="https://github.com/manavortex/alfred-fandom-search/blob/master/README.md", 
                 valid=True
@@ -62,18 +82,26 @@ class AlfredFandomSearch(object):
         except IndexError: 
             searchResults = {}
         
+        #print('\nresults')
         for result in searchResults:
-
-            title=result["title"]
-            soup = BeautifulSoup(result["snippet"])
-            snipText = (soup.get_text()[:75] + "..")            
-            subtitle=snipText
+           
+            #print(result)
             
-            url=result["url"]
+            title=result['title']
+            soup = BeautifulSoup(result["snippet"], features="html.parser")
+            snipText = (soup.get_text()[:75] + "..")            
+            subtitle=snipText.encode('utf-8')
+            
+            url=''
+            try:
+                url = result['arg']
+            except: 
+                url = ('https://%s.fandom.com/wiki/' % DEFAULT_WIKI) + title.encode('utf-8')
+
             self.wf.add_item(
-                title, 
+                title,
                 subtitle=subtitle,
-                arg=url,                 
+                arg=url,
                 valid=True
             )#, icon=header_image_url, icontype='path')
             
